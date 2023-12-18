1f52d0fe0ee19275dff27026dfec41b00a98f263
Enzo <enzo.daval@etu.univ-cotedazur.fr>
2023-12-18 10:30:46 +0100

    Refactor + setup demo

---
+++ DataProcessing/Classes/Eleve.py
@@ -0,0 +1,6 @@
+class Eleve:
+    def __init__(self, nom, prenom, presence, certitude):
+        self.nom = nom
+        self.prenom = prenom
+        self.presence = presence
+        self.certitude = certitude
