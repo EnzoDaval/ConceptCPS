c8b612a4fcbd3f95864d41b351c0b248f10be451
ThomasTricot <thomas.tricot@etu.unice.fr>
2023-12-19 16:32:22 +0100

    INFLUX adding dispositif and data

--- InfluxDB/client.py
+++ InfluxDB/client.py
@@ -1,6 +1,8 @@
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
-from eleve import *
+
+from dispositif import *
+from data import *
 
 url = "https://us-east-1-1.aws.cloud2.influxdata.com"
 token = "CHPF60Fmc4WWf1tNKXqjRUVm2we9FU3YTqAJE23oNmURgJcMwnacqN5UIRd_PdM01fq-sqM-lIJH6jOz2meqeg=="
@@ -10,16 +12,28 @@ bucket = "CyberPhysique"
 client = InfluxDBClient(url=url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
-def envoyer_donnees(eleves, nom_cours):
-    for eleve in eleves:
-        point = Point("cours")\
-            .tag("nomCours", nom_cours)\
-            .tag("nom", eleve.nom)\
-            .tag("prenom", eleve.prenom)\
-            .field("presence", eleve.presence)\
-            .field("certitude", eleve.certitude)
-        write_api.write(bucket=bucket, org=org, record=point)
+def envoyer_donnees_dispositif(disositif):
+    point = Point("dispositif")\
+        .tag("ID_Dispositif", disositif.id)\
+        .tag("Nom_Dispositif", disositif.nom)\
+        .tag("Salle", disositif.salle)\
+        .field("Horaires", disositif.horaire)\
+        .field("Noms_Eleves", disositif.eleves)
+    write_api.write(bucket=bucket, org=org, record=point)
 
-eleves = [Eleve("Dupont", "Alice", True, 90), Eleve("Tricot", "Bob", False, 70)]
-envoyer_donnees(eleves, "Cours A")
+def envoyer_donnes_data(data):
+    donnees_concatenees = ",".join(data.data)
+    point = Point("donnees") \
+        .tag("ID_Dispositif", data.id) \
+        .field("Donnes", donnees_concatenees)
+    write_api.write(bucket=bucket, org=org, record=point)
+
+
+#COMMENT UTILISER :
+# dispositif = Dispositif(150, "Camera", "E303", "13h30-15h30", "Enzo, Morgane, Emilien")
+# envoyer_donnees_dispositif(dispositif)
+#
+# data = Data(15, ["test", "dispositif", "comment", "548"])
+# envoyer_donnes_data(data)
+
 client.close()
---
+++ InfluxDB/dispositif.py
@@ -0,0 +1,7 @@
+class Dispositif:
+    def __init__(self, id, nom, salle, horaire, eleves):
+        self.id = id
+        self.nom = nom
+        self.salle = salle
+        self.horaire = horaire
+        self.eleves = eleves
